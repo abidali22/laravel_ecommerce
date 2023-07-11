d878b2ebd706c4ba6132e33a2fdf5ebcfe1d22b0
Dev Abid <abidali22>
2023-07-11 23:36:16 +0500

    plane code without breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('user_type')->default('USR')->comment('ADM for admin USR for normal user');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
